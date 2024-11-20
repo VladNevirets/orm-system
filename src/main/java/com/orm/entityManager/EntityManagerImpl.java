5e3b41ea7663a506f39678cbeebae74417fe50e4
Vlad Nevirets <neviretsv@gmail.com>
2024-11-20 14:28:00 +0200

    Create Convertor class

--- src/main/java/com/orm/entityManager/EntityManagerImpl.java
+++ src/main/java/com/orm/entityManager/EntityManagerImpl.java
@@ -10,6 +10,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static com.orm.Convertor.convertValue;
+
 public class EntityManagerImpl implements EntityManager {
     Connection conn;
 
@@ -90,27 +92,5 @@ public class EntityManagerImpl implements EntityManager {
         }
 
     }
-    private Object convertValue(Object value, Class<?> targetType) {
-        if (value == null) {
-            return null;
-        }
 
-        if (targetType.isAssignableFrom(value.getClass())) {
-            return value;
-        }
-
-        if (targetType == Integer.class || targetType == int.class) {
-            return ((Number) value).intValue();
-        } else if (targetType == Long.class || targetType == long.class) {
-            return ((Number) value).longValue();
-        } else if (targetType == Double.class || targetType == double.class) {
-            return ((Number) value).doubleValue();
-        } else if (targetType == Float.class || targetType == float.class) {
-            return ((Number) value).floatValue();
-        } else if (targetType == String.class) {
-            return value.toString();
-        }
-
-        throw new IllegalArgumentException("Cannot convert value of type " + value.getClass() + " to " + targetType);
-    }
 }
