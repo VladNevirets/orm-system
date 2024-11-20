7a065c60084424e29bb0bfcfaf7102df248b041f
Vlad Nevirets <neviretsv@gmail.com>
2024-11-20 04:57:12 +0200

    Update main file

--- src/main/java/com/orm/Main.java
+++ src/main/java/com/orm/Main.java
@@ -1,7 +1,24 @@
 package com.orm;
 
+import com.orm.config.DatabaseConfig;
+import com.orm.entityManager.EntityManager;
+import com.orm.entityManager.EntityManagerImpl;
+import com.orm.test.User;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        DatabaseConfig config = new DatabaseConfig();
+        EntityManager<User> entityManager = new EntityManagerImpl(config.getConnection());
+        User user = new User(Long.valueOf(1),"John");
+        entityManager.create(user);
+        User readUser = (User) entityManager.read(User.class,1);
+        System.out.println(readUser);
+        User updateUser = new User(Long.valueOf(1),"Johnathan");
+        entityManager.update(updateUser);
+        User readUpdatedUser = (User) entityManager.read(User.class,1);
+        System.out.println(readUpdatedUser);
+        entityManager.delete(readUpdatedUser);
     }
 }
