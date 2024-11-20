86fa9bc5380f837e3c4e465aee013ab0ac39c829
Vlad Nevirets <neviretsv@gmail.com>
2024-11-20 04:56:03 +0200

    Create database connection configuration

--- src/main/java/com/orm/config/DatabaseConfig.java
+++ src/main/java/com/orm/config/DatabaseConfig.java
@@ -1,20 +1,43 @@
-package com.orm;
+package com.orm.config;
 
-import com.orm.EntityManager.EntityManager;
-import com.orm.EntityManager.EntityManagerImpl;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
-public class ConnectDb {
-    public void connect(String url,Object entity) {
-        try {
-            Connection conn = DriverManager.getConnection(url);
-            EntityManager<entity> entityManager = new EntityManagerImpl();
-        }catch(SQLException e) {
-            e.printStackTrace();
+public class DatabaseConfig {
+    private String url;
+    private String username;
+    private String password;
+    private String driver;
+
+    public DatabaseConfig() {
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("app.properties")) {
+            if (input == null) {
+                throw new RuntimeException("File 'app.properties' not found in resources!");
+            }
+            properties.load(input);
+
+            this.url = properties.getProperty("datasource.url");
+            this.username = properties.getProperty("datasource.username");
+            this.password = properties.getProperty("datasource.password");
+            this.driver = properties.getProperty("datasource.driver");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load database configuration", e);
         }
-
     }
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Database driver not found", e);
+        }
+        return DriverManager.getConnection(url, username, password);
+    }
+
 }
